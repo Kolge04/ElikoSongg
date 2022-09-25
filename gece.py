ea8f4afc9171a3d8f3648aa934be5dd5ff0fda38
Nixhad Axhmedov <108715556+NikhadAxhmedov@users.noreply.github.com>
2022-09-25 04:52:21 +0400

    Create gece.py

---
+++ gece.py
@@ -0,0 +1,487 @@
+#################################
+# Naz Tagger Bot #
+#################################
+# Repo Sahibi - BenimKiller 
+# Telegram - t.me/lovelesslifee
+# Telegram - t.me/Naz_Vanessa
+##################################
+import heroku3
+import random
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.events import StopPropagation
+from config import client, USERNAME, log_qrup, startmesaj, qrupstart, komutlar, sahib, support
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+
+
+anlik_calisan = []
+gece_tag = []
+
+
+#tektag
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def cancel(event):
+  global gece_tag
+  gece_tag.remove(event.chat_id)
+  
+  
+# Başlanğıc Mesajı
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  if event.is_private:
+    async for usr in client.iter_participants(event.chat_id):
+     ad = f"[{usr.first_name}](tg://user?id={usr.id}) "
+     await client.send_message(log_qrup, f"ℹ️ **Yeni Kullanıcı -** {ad}")
+     return await event.reply(f"{ad} {startmesaj}", buttons=(
+                      [
+                       Button.inline("🎛 Əmrlər", data="komutlar")
+                      ],
+                      [Button.url('🌱 Məni qrupa əlavə et', f'https://t.me/Sofi_Tag_Bot?startgroup=a')],
+                      [Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44'),
+                       Button.url('🙇🏻‍♀️ Sahibə', f'https://t.me/Avaraqizdidaa')]
+                    ),
+                    link_preview=False)
+
+
+  if event.is_group:
+    return await client.send_message(event.chat_id, f"{qrupstart}")
+
+# Başlanğıc Button
+@client.on(events.callbackquery.CallbackQuery(data="start"))
+async def handler(event):
+    async for usr in client.iter_participants(event.chat_id):
+     ad = f"[{usr.first_name}](tg://user?id={usr.id}) "
+     await event.edit(f"{ad} {startmesaj}", buttons=(
+                      [
+                       Button.inline("🎛 əmrlər", data="komutlar")
+                      ],
+                      [Button.url('🌱 Məni qrupa əlavə et', f'https://t.me/Sofi_Tag_Bot?startgroup=a')],
+                      [Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44'),
+                       Button.url('🙇🏻‍♀️ Sahibə', f'https://t.me/Avaraqizdidaa')]
+                    ),
+                    link_preview=False)
+
+# gece kusu
+@client.on(events.callbackquery.CallbackQuery(data="komutlar"))
+async def handler(event):
+    await event.edit(f"{komutlar}", buttons=(
+                      [
+                      Button.inline("◀️ Geri", data="start")
+                      ]
+                    ),
+                    link_preview=False)
+
+
+@client.on(events.NewMessage(pattern="^/all ?(.*)"))
+async def mentionall(event):
+  global gece_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{admin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajlar üçün üyeleri tağ edə bilmərəm! (qrupa əlavə edilməzdən əvvəl göndərilən mesajlar)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın🙄__")
+  else:
+    return await event.respond("__Bir mesajı yanıtlayın vəya başqalarını tağ etmək üçün mənə bir mətn verin!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ User tağ prosesi başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                      Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  ) 
+    gece_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f" [{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in gece_tag:
+        await event.respond("⛔ Tağ prosesi dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+    
+
+#########################
+
+
+@client.on(events.NewMessage(pattern="^/admin ?(.*)"))
+async def mentionalladmin(event):
+  global yarasatagger
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{admin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın🙄__")
+  else:
+    return await event.respond("__Tağ etmək üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Admin tağ prosesi başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  ) 
+    Yarasatagger.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in gece_tag:
+        await event.respond("⛔ Admin tağ prosesi dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+    
+
+#########################
+
+
+@client.on(events.NewMessage(pattern="^/tektag ?(.*)"))
+async def tektag(event):
+  global gece_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{noadmin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın🙄__")
+  else:
+    return await event.respond("__Tağ etmək üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Tək-Tək tağ prosesi başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  ) 
+    gece_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in gece_tag:
+        await event.respond("⛔ Tək-Tək tağ prosesi dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+    
+
+#########################
+
+
+anlik_calisan = []
+
+tekli_calisan = []
+
+
+
+
+emoji = " ❤️ 🧡 💛 💚 💙 💜 🖤 🤍 🤎 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🤩 🥳 😏 😒 " \
+        "😞 😔 😟 😕 🙁 😣 😖 😫 😩 🥺 😢 😭 😤 😠 😡  🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🤗 🤔 🤭 🤫 🤥 😶 😐 😑 😬 🙄 " \
+        "😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 🤐 🥴 🤢 🤮 🤧 😷 🤒 🤕 🤑 🤠 😈 👿 👹 👺 🤡  👻 💀 👽 👾 🤖 🎃 😺 😸 😹 " \
+        "😻 😼 😽 🙀 😿 😾".split(" ")
+
+
+@client.on(events.NewMessage(pattern="^/etag ?(.*)"))
+async def etag(event):
+  global gece_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{noadmin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın🙄__")
+  else:
+    return await event.respond("__Tağ etmək üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Emoji ilə tağ prosesi başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  ) 
+    gece_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
+      if event.chat_id not in gece_tag:
+        await event.respond("⛔ Emoji tağ prosesi dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+    
+
+#########################
+
+
+
+soz = (
+'𝐾𝑎𝑙𝑏𝑖 𝑔ü𝑧𝑒𝑙 𝑜𝑙𝑎𝑛ı𝑛 𝑔ö𝑧ü𝑛𝑑𝑒𝑛 𝑦𝑎ş 𝑒𝑘𝑠𝑖𝑘 𝑜𝑙𝑚𝑎𝑧𝑚ış', 
+'İ𝑦𝑖𝑦𝑖𝑚 𝑑𝑒𝑠𝑒𝑚 𝑖𝑛𝑎𝑛𝑎𝑐𝑎𝑘 𝑜 𝑘𝑎𝑑𝑎𝑟 ℎ𝑎𝑏𝑒𝑟𝑠𝑖𝑧 𝑏𝑒𝑛𝑑𝑒𝑛', 
+'𝐺üç𝑙ü 𝑔ö𝑟ü𝑛𝑒𝑏𝑖𝑙𝑖𝑟𝑖𝑚 𝑎𝑚𝑎 𝑖𝑛𝑎𝑛 𝑏𝑎𝑛𝑎 𝑦𝑜𝑟𝑔𝑢𝑛𝑢𝑚', 
+'Ö𝑚𝑟ü𝑛ü𝑧ü 𝑠𝑢𝑠𝑡𝑢𝑘𝑙𝑎𝑟ı𝑛ı𝑧ı 𝑑𝑢𝑦𝑎𝑛  𝑏𝑖𝑟𝑖𝑦𝑙𝑒 𝑔𝑒ç𝑖𝑟𝑖𝑛', 
+'𝐻𝑎𝑦𝑎𝑡 𝑖𝑙𝑒𝑟𝑖𝑦𝑒 𝑏𝑎𝑘ı𝑙𝑎𝑟𝑎𝑘 𝑦𝑎ş𝑎𝑛ı𝑟 𝑔𝑒𝑟𝑖𝑦𝑒 𝑏𝑎𝑘𝑎𝑟𝑎𝑘 𝑎𝑛𝑙𝑎şı𝑙ı𝑟', 
+'𝐴𝑟𝑡ı𝑘 ℎ𝑖ç𝑏𝑖𝑟 ş𝑒𝑦 𝑒𝑠𝑘𝑖𝑠𝑖 𝑔𝑖𝑏𝑖 𝑑𝑒ğ𝑖𝑙 𝐵𝑢𝑛𝑎 𝑏𝑒𝑛𝑑𝑒 𝑑𝑎ℎ𝑖𝑙𝑖𝑚', 
+'𝐾ı𝑦𝑚𝑒𝑡 𝑏𝑖𝑙𝑒𝑛𝑒 𝑔ö𝑛ü𝑙𝑑𝑒 𝑣𝑒𝑟𝑖𝑙𝑖𝑟 ö𝑚ü𝑟𝑑𝑒', 
+'𝐵𝑖𝑟 ç𝑖ç𝑒𝑘𝑙𝑒 𝑔ü𝑙𝑒𝑟 𝑘𝑎𝑑ı𝑛 𝑏𝑖𝑟 𝑙𝑎𝑓𝑙𝑎 ℎü𝑧ü𝑛', 
+'𝑈𝑠𝑙ü𝑝 𝑘𝑎𝑟𝑎𝑘𝑡𝑒𝑟𝑖𝑑𝑖𝑟 𝑖𝑛𝑠𝑎𝑛ı𝑛', 
+'𝐻𝑒𝑟 ş𝑒𝑦𝑖 𝑏𝑖𝑙𝑒𝑛 𝑑𝑒ğ𝑖𝑙 𝑘ı𝑦𝑚𝑒𝑡 𝑏𝑖𝑙𝑒𝑛 𝑖𝑛𝑠𝑎𝑛𝑙𝑎𝑟 𝑜𝑙𝑠𝑢𝑛 ℎ𝑎𝑦𝑎𝑡ı𝑛ı𝑧𝑑𝑎', 
+'𝑀𝑒𝑠𝑎𝑓𝑒 𝑖𝑦𝑖𝑑𝑖𝑟 𝑁𝑒 ℎ𝑎𝑑𝑑𝑖𝑛𝑖 𝑎ş𝑎𝑛 𝑜𝑙𝑢𝑟 𝑛𝑒 𝑑𝑒 𝑐𝑎𝑛ı𝑛ı 𝑠ı𝑘𝑎𝑛', 
+'𝑌ü𝑟𝑒ğ𝑖𝑚𝑖𝑛 𝑡𝑎𝑚 𝑜𝑟𝑡𝑎𝑠ı𝑛𝑑𝑎 𝑏ü𝑦ü𝑘 𝑏𝑖𝑟 𝑦𝑜𝑟𝑔𝑢𝑛𝑙𝑢𝑘 𝑣𝑎𝑟', 
+'𝑉𝑒𝑟𝑖𝑙𝑒𝑛 𝑑𝑒ğ𝑒𝑟𝑖𝑛 𝑛𝑎𝑛𝑘ö𝑟ü 𝑜𝑙𝑚𝑎𝑦ı𝑛 𝑔𝑒𝑟𝑖𝑠𝑖 ℎ𝑎𝑙𝑙𝑜𝑙𝑢𝑟', 
+'𝐻𝑒𝑚 𝑔üç𝑙ü 𝑜𝑙𝑢𝑝 ℎ𝑒𝑚 ℎ𝑎𝑠𝑠𝑎𝑠 𝑘𝑎𝑙𝑝𝑙𝑖 𝑏𝑖𝑟𝑖 𝑜𝑙𝑚𝑎𝑘 ç𝑜𝑘 𝑧𝑜𝑟', 
+'𝑀𝑢ℎ𝑡𝑎ç 𝑘𝑎𝑙ı𝑛 𝑦ü𝑟𝑒ğ𝑖 𝑔ü𝑧𝑒𝑙 𝑖𝑛𝑠𝑎𝑛𝑙𝑎𝑟𝑎', 
+'İ𝑛𝑠𝑎𝑛 𝑎𝑛𝑙𝑎𝑑ığı 𝑣𝑒 𝑎𝑛𝑙𝑎şı𝑙𝑑ığı 𝑖𝑛𝑠𝑎𝑛𝑑𝑎 ç𝑖ç𝑒𝑘 𝑎ç𝑎𝑟', 
+'İ𝑠𝑡𝑒𝑦𝑒𝑛 𝑑𝑎ğ𝑙𝑎𝑟ı 𝑎ş𝑎𝑟 𝑖𝑠𝑡𝑒𝑚𝑒𝑦𝑒𝑛 𝑡ü𝑚𝑠𝑒ğ𝑖 𝑏𝑖𝑙𝑒 𝑔𝑒ç𝑒𝑚𝑒𝑧', 
+'İ𝑛ş𝑎𝑙𝑙𝑎ℎ 𝑠𝑎𝑏ı𝑟𝑙𝑎 𝑏𝑒𝑘𝑙𝑒𝑑𝑖ğ𝑖𝑛 ş𝑒𝑦 𝑖ç𝑖𝑛 ℎ𝑎𝑦ı𝑟𝑙ı 𝑏𝑖𝑟 ℎ𝑎𝑏𝑒𝑟 𝑎𝑙ı𝑟𝑠ı𝑛', 
+'İ𝑦𝑖 𝑜𝑙𝑎𝑛 𝑘𝑎𝑦𝑏𝑒𝑡𝑠𝑒 𝑑𝑒 𝑘𝑎𝑧𝑎𝑛ı𝑟', 
+'𝐺ö𝑛𝑙ü𝑛ü𝑧𝑒 𝑎𝑙𝑑ığı𝑛ı𝑧 𝑔ö𝑛𝑙ü𝑛ü𝑧ü 𝑎𝑙𝑚𝑎𝑦ı 𝑏𝑖𝑙𝑠𝑖𝑛', 
+'𝑌𝑖𝑛𝑒 𝑦ı𝑟𝑡ı𝑘 𝑐𝑒𝑏𝑖𝑚𝑒 𝑘𝑜𝑦𝑚𝑢ş𝑢𝑚 𝑢𝑚𝑢𝑑𝑢', 
+'Ö𝑙𝑚𝑒𝑘 𝐵𝑖 ş𝑒𝑦 𝑑𝑒ğ𝑖𝑙 𝑦𝑎ş𝑎𝑚𝑎𝑚𝑎𝑘 𝑘𝑜𝑟𝑘𝑢𝑛ç', 
+'𝑁𝑒 𝑖ç𝑖𝑚𝑑𝑒𝑘𝑖 𝑠𝑜𝑘𝑎𝑘𝑙𝑎𝑟𝑎 𝑠ığ𝑎𝑏𝑖𝑙𝑑𝑖𝑚 𝑁𝑒 𝑑𝑒 𝑑ış𝑎𝑟ı𝑑𝑎𝑘𝑖 𝑑ü𝑛𝑦𝑎𝑦𝑎', 
+'İ𝑛𝑠𝑎𝑛 𝑠𝑒𝑣𝑖𝑙𝑚𝑒𝑘𝑡𝑒𝑛 ç𝑜𝑘 𝑎𝑛𝑙𝑎şı𝑙𝑚𝑎𝑦ı 𝑖𝑠𝑡𝑖𝑦𝑜𝑟𝑑𝑢 𝑏𝑒𝑙𝑘𝑖 𝑑𝑒', 
+'𝐸𝑘𝑚𝑒𝑘 𝑝𝑎ℎ𝑎𝑙ı 𝑒𝑚𝑒𝑘 𝑢𝑐𝑢𝑧𝑑𝑢', 
+'𝑆𝑎𝑣𝑎ş𝑚𝑎𝑦ı 𝑏ı𝑟𝑎𝑘ı𝑦𝑜𝑟𝑢𝑚 𝑏𝑢𝑛𝑢 𝑣𝑒𝑑𝑎 𝑠𝑎𝑦'
+) 
+
+
+@client.on(events.NewMessage(pattern="^/stag ?(.*)"))
+async def stag(event):
+  global yarasatagger
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{admin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("_Tağ mesajı yazmadın🙄__")
+  else:
+    return await event.respond("__Tağ etmək üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Sözlərlə tağ prosesi başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  ) 
+    gece_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(soz)}](tg://user?id={usr.id}) "
+      if event.chat_id not in yarasatagger:
+        await event.respond("⛔ Söz ilə tağ prosesi dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+    
+#########################
+
+renk = "🔴 🟠 🟡 🟢 🔵 🟣 🟤 ⚫ ⚪ " .split(" ") 
+        
+
+@client.on(events.NewMessage(pattern="^/rtag ?(.*)"))
+async def rtag(event):
+  global gece_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{admin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın🙄__")
+  else:
+    return await event.respond("__Tağ etmək üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Rəng ilə tağ prosesi başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/lovelesslifee')
+                      ]
+                    )
+                  ) 
+    yarasatagger.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(renk)}](tg://user?id={usr.id}) "
+      if event.chat_id not in yarasatagger:
+        await event.respond("⛔ Rəngli tağ prosesi dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('🤖 Digər Botlar', f'https://t.me/Rahid_44')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 3:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+    
+#########################
+
+
+
+
+
+print(">> Bot işlək vəziyyətdədir 🚀 əlaqə üçün @Rahid_2003 <<")
+client.run_until_disconnected()
+run_until_disconnected()
