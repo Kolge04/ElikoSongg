e54c2f4554a04db887372bf08b0d0e0df7abb9a0
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-09-20 19:57:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     BOT_TOKEN = os.getenv("6344855563:AAER4AWp7vs2vsWJbVXApq-Pb9stw2uf-C0")
     API_ID = int(os.getenv("14965050"))
-    API_HASH = os.getenv(("38bab2dab10fc1b6a9ba0bf683fd7048")
+    API_HASH = os.getenv("38bab2dab10fc1b6a9ba0bf683fd7048")
     BOT_OWNER = os.environ.get("ElikoAndMee")
     BOT_USERNAME = os.environ.get("ElikoSongBot")
     PLAYLIST_NAME = os.environ.get("𝙀𝙡𝙞𝙠𝙤 𝙋𝙡𝙖𝙮𝙡𝙞𝙨𝙩")
