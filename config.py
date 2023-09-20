090210121df7d8a2abc9692232a23247f0ba389b
ElikoAndMee <legendmuzkao99@gmail.com>
2023-09-20 18:23:53 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
+    BOT_TOKEN = os.environ.get("6344855563:AAER4AWp7vs2vsWJbVXApq-Pb9stw2uf-C0")
+    API_ID = int(os.environ.get("14965050"))
+    API_HASH = os.environ.get("38bab2dab10fc1b6a9ba0bf683fd7048")
+    BOT_OWNER = os.environ.get("")
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
     PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
